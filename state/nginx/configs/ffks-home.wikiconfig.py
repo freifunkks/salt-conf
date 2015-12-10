697963e8fda1a62b71c9b56ee152a60f1c867125
Jonas Platte <mail@jonasplatte.de>
2015-12-10 23:15:02 +0100

    Fix old hardcoded static directory in meta tags

--- state/nginx/configs/ffks-home.wikiconfig.py
+++ state/nginx/configs/ffks-home.wikiconfig.py
@@ -115,28 +115,29 @@ class Config(multiconfig.DefaultConfig):
 
   # Favicons for every device
   html_head = '''
-  <link rel="apple-touch-icon" sizes="57x57" href="/wikistatic/icons/apple-touch-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="/wikistatic/icons/apple-touch-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="/wikistatic/icons/apple-touch-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="/wikistatic/icons/apple-touch-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="/wikistatic/icons/apple-touch-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="/wikistatic/icons/apple-touch-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="/wikistatic/icons/apple-touch-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="/wikistatic/icons/apple-touch-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="/wikistatic/icons/apple-touch-icon-180x180.png">
-  <link rel="icon" type="image/png" href="/wikistatic/icons/favicon-32x32.png" sizes="32x32">
-  <link rel="icon" type="image/png" href="/wikistatic/icons/android-chrome-192x192.png" sizes="192x192">
-  <link rel="icon" type="image/png" href="/wikistatic/icons/favicon-96x96.png" sizes="96x96">
-  <link rel="icon" type="image/png" href="/wikistatic/icons/favicon-16x16.png" sizes="16x16">
-  <link rel="manifest" href="/wikistatic/icons/manifest.json">
-  <link rel="shortcut icon" href="/wikistatic/icons/favicon.ico">
-  <meta name="apple-mobile-web-app-title" content="Freifunk Kassel">
-  <meta name="application-name" content="Freifunk Kassel">
-  <meta name="msapplication-TileColor" content="#ffc40d">
-  <meta name="msapplication-TileImage" content="/wikistatic/icons/mstile-144x144.png">
-  <meta name="msapplication-config" content="/wikistatic/icons/browserconfig.xml">
-  <meta name="theme-color" content="#ffffff">
-  <script type="text/javascript" src="/wikistatic/ffks/js/jquery-2.1.4.min.js"></script>'''
+    <link rel="apple-touch-icon" sizes="57x57" href="{0}/icons/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="{0}/icons/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="{0}/icons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="{0}/icons/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="{0}/icons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="{0}/icons/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="{0}/icons/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="{0}/icons/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="{0}/icons/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="{0}/icons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="{0}/icons/android-chrome-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="{0}/icons/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="{0}/icons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="{0}/icons/manifest.json">
+    <link rel="shortcut icon" href="{0}/icons/favicon.ico">
+    <meta name="apple-mobile-web-app-title" content="Freifunk Kassel">
+    <meta name="application-name" content="Freifunk Kassel">
+    <meta name="msapplication-TileColor" content="#ffc40d">
+    <meta name="msapplication-TileImage" content="{0}/icons/mstile-144x144.png">
+    <meta name="msapplication-config" content="{0}/icons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <script type="text/javascript" src="{0}/ffks/js/jquery-2.1.4.min.js"></script>
+    '''.format(url_prefix_static)
 
   # Enforce custom theme
   theme_default = u'ffks'
