7cb7fc60f0c1da90cfb02627eed09f89decdb094
Felix Möller <git@felix-moeller.net>
2015-12-15 06:43:34 +0100

    Dialogs now use salt's minions

--- bootstrap/bootstrap-minion.sh
+++ bootstrap/bootstrap-minion.sh
@@ -26,49 +26,55 @@
 #grep ${minion_local} ${minion_file} 1>/dev/null && echo "Local minion mode already enabled." || ( sed -i "/#file_client: remote/a ${minion_local}" ${minion_file}; echo "Enabled local minion mode." )
 #
 ## Install hostname selection prerequisites
-#apt-get install -y python-pip
-#pip install shyaml
+#apt-get install -y python-pip dialog
+#pip2 install shyaml
 
 # Dialog options
 export DIALOGRC="$PWD/dialogrc"
-minion_list="$PWD/testminions.yaml"
+minion_pre="$PWD/../pillar/minion-"
+#minion_list=$(pre='pillar/minion-'; for i in ${pre}*; do echo $i | sed 's|'$pre'\(\w\+\)\.sls|\1:|'; cat $i | sed 's/^/  /'; done)
+minion_list=$(for i in ${minion_pre}*; do echo $i | sed 's|'${minion_pre}'\(\w\+\)\.sls|\1|'; done)
 dialog_choice=$(mktemp)
 dialog_min=7
 domain_inner="ffks"
 domain_outer="${domain_inner}.de"
 
-# Select minion's role
-# (Gateway / webserver)
+# Select minion's hostname and
+# thus choosing it's role and packages
 
-function select_server_role() {
-	roles_dialog=$(cat ${minion_list} | shyaml keys | sed = | sed 's/s$//' | sed -r 's/^([a-z])/\U\1/' | sed ':a;N;$!ba;s/\n/ /g')
-	roles_num=$(cat ${minion_list} | shyaml keys | wc -l)
-	dialog --menu "Select server role:" $((7+${roles_num})) 23 ${roles_num} ${roles_dialog} 2> ${dialog_choice}
-	roles_choice=$(<"${dialog_choice}")
-	role=$(cat ${minion_list} | shyaml keys | sed -n ${roles_choice}p)
-	select_server_hostname
+function dia_host_available () {
+	dialog --msgbox "Hostname seems to be free. Starting salt setup now." 6 30
 }
 
-function select_server_hostname() {
+function dia_host_unavailable () {
+	dialog --msgbox "Hostname (${1}) is not available within salt repository. Please choose another one." 6 $((44+${#1}))
+}
+
+function dia_host_already_used () {
+	dialog --msgbox "Hostname seems to be already in use. Please select another one." 6 40
+}
+
+function select_server_host() {
 	# Select hostname from gateways / webservers
-	hostnames_dialog=$(cat ${minion_list} | shyaml get-values ${role} | sed = | sed 's/s$//' | sed ':a;N;$!ba;s/\n/ /g')
-	hostnames_num=$(cat ${minion_list} | shyaml get-values ${role} | wc -l)
+	hostnames_dialog=$(echo "${minion_list}" | sed = | sed ':a;N;$!ba;s/\n/ /g')
+	hostnames_num=$(echo "${minion_list}" | wc -l)
+	echo ${hostnames_dialog} 
+	echo ${hostnames_num}
 	dialog --menu "Select servers hostname:" $((${dialog_min}+${hostnames_num})) 29 ${hostnames_num} ${hostnames_dialog} 2> ${dialog_choice}
 	hostnames_choice=$(<"${dialog_choice}")
-	hostname=$(cat ${minion_list} | shyaml get-values ${role} | sed -n ${hostnames_choice}p)
+	hostname=$(echo "${minion_list}" | sed -n ${hostnames_choice}p)
 
 	# Check if hostname is already in use
-	# TODO Check network connectivity beforehand?
-	ping -W 2 -c1 ${hostname}.${domain_outer} 2>1 >/dev/null && ( dialog --msgbox "Hostname seems to be already in use. Please select another one." 6 40; select_server_hostname ) || dialog --msgbox "Hostname seems to be free. Starting salt setup now." 6 30
+	ping -W 2 -c1 ${hostname}.${domain_outer} 2>1 >/dev/null && ( dia_host_already_used; select_server_host ) || dia_hostname_available
 }
 
 if [[ -z $1 ]]; then
-	select_server_role
+	select_server_host
 else
 	# TODO Unfuck variable scope
 	hostname="$1"
-	cat ${minion_list} | grep -c "^\s*\- ${hostname}$" >/dev/null || ( dialog --msgbox "Hostname (${hostname}) is not available within salt repository. Please choose another one." 6 $((44+${#hostname})); select_server_role )
-	ping -W 2 -c1 ${hostname}.${domain_outer} 2>1 >/dev/null && ( dialog --msgbox "Hostname seems to be already in use. Please select another one." 6 40; select_server_role )
+	echo "${minion_list}" | grep -c "^${hostname}$" >/dev/null || ( dia_host_unavailable ${hostname}; select_server_host )
+	ping -W 2 -c1 ${hostname}.${domain_outer} 2>1 >/dev/null && ( dia_host_already_used; select_server_host )
 	echo ${hostname}.${domain_outer}
 fi
 
