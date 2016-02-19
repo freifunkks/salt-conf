d3591bf7b9a4920f2fba222793e1295a69f6be66
Jonas Platte <mail@jonasplatte.de>
2016-02-19 01:05:38 +0100

    You shall not use non-existing options.

--- state/openvpn/airvpn/down.sh
+++ state/openvpn/airvpn/down.sh
@@ -5,4 +5,4 @@ fastd_interface="ffks-vpn"
 
 ip rule del table ffks from {{ pillar.minions[grains.id].bat_ip }} priority 9970
 
-iptables --table nat --remove POSTROUTING --out-interface "$openvpn_interface" -j MASQUERADE
+iptables --table nat --delete POSTROUTING --out-interface "$openvpn_interface" -j MASQUERADE
