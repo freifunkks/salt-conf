8b6e078dd83ad13d2c754dd8ec59252e96a19432
Felix Möller <git@felix-moeller.net>
2017-11-19 19:31:54 +0100

    Use values aggregated in space api

--- state/graphite/fd-spacestats.py
+++ state/graphite/fd-spacestats.py
@@ -8,11 +8,6 @@ import socket
 import time
 
 
-SERVER_FD = 'flipdot.org'
-API_FD = {
-    'status': '/spacestatus/status.json',
-}
-
 SERVER_FD_NG = 'api.flipdot.org'
 API_FD_NG = {
     'api': '',
@@ -39,8 +34,11 @@ def write_to_graphite(data, prefix='fd.space'):
             elif type(value) is list:
                 for item in value:
                     # Beverages
-                    if 'location' in item:
+                    if 'location' in item and 'name' in item:
                         line = "%s.%s.%s.%s.%s %s %s\n" % (prefix, key, item['location'], item['name'], item['unit'], float(item['value']), now)
+                    # Heater
+                    elif not 'name' in item:
+                        line = "%s.%s.%s.%s %s %s\n" % (prefix, key, item['location'], item['unit'], float(item['value']), now)
                     else:
                         line = "%s.%s.%s.%s %s %s\n" % (prefix, key, item['name'], item['unit'], float(item['value']), now)
                     s.sendall(line.encode('latin-1'))
@@ -53,56 +51,7 @@ def write_to_graphite(data, prefix='fd.space'):
 def main():
     update = {}
 
-    # Get user data
-    conn = httplib.HTTPSConnection(SERVER_FD)
-    conn.request('GET', API_FD['status'])
-
-    r = conn.getresponse()
-    if r.status is not 200:
-        print r.status, r.reason, "for", SERVER_FD
-    else:
-        data = json.loads(r.read())
-
-        try:
-            known_users = len(data['known_users'])
-        except KeyError:
-            known_users = 0
-
-        try:
-            unknown_users = data['unknown_users']
-        except KeyError:
-            unknown_users = 0
-
-        try:
-            update['heater_setpoint'] = data['temperature_setpoint']
-        except:
-            pass
-
-        try:
-            update['heater_temperature'] = data['temperature_realvalue']
-        except:
-            pass
-
-        try:
-            update['heater_valve'] = data['heater_valve']
-        except:
-            pass
-
-        try:
-            is_open = 1 if data['open'] else 0
-        except KeyError:
-            is_open = 0
-
-        all_users = known_users + unknown_users
-
-        update['users_all'] = all_users
-        update['users_known'] = known_users
-        update['users_unknown'] = unknown_users
-        update['is_open'] = is_open
-
-    conn.close()
-
-    # Get the API stuff like co2 and power consumption
+    # Get values from the space api
     conn = httplib.HTTPSConnection(SERVER_FD_NG)
     conn.request('GET', API_FD_NG['api'])
 
@@ -111,17 +60,52 @@ def main():
         print r.status, r.reason, "for", SERVER_FD_NG
     else:
         data = json.loads(r.read())
+        sensors = data['state']['sensors']
+
+        # user values
+        try:
+            known_users = len(sensors['people_now_present'][0]['names'].split(','))
+        except:
+            known_users = 0
+
+        try:
+            unknown_users = sensors['people_now_present'][0]['value'] - known_users
+        except:
+            unknown_users = 0
+
+        try:
+            update['heater_setpoint'] = sensors['heater_set_point'][0]['value']
+        except:
+            update['heater_setpoint'] = 0
+
+        try:
+            update['heater_valve'] = sensors['heater_valve'][0]['value']
+        except:
+            update['heater_valve'] = 0
+
+        try:
+            update['temperature'] = sensors['temperature']
+        except:
+            pass
+
+        is_open = 1 if data['open'] else 0
+        all_users = known_users + unknown_users
+
+        update['users_all'] = all_users
+        update['users_known'] = known_users
+        update['users_unknown'] = unknown_users
+        update['is_open'] = is_open
 
         # co2 values: 0-3000 (unit ppm)
         try:
-            co2 = data['state']['sensors']['co2'][0]['value']
+            co2 = sensors['co2'][0]['value']
         except KeyError:
             co2 = 0
         update['co2'] = co2
 
         # power
         try:
-            power = data['state']['sensors']['power']
+            power = sensors['power']
             power_api = []
             for i in power:
                 tmp = {}
@@ -136,7 +120,7 @@ def main():
             pass
 
         # drinks storage 0-XX per drink (unit crates)
-        update['drinks'] = data['state']['sensors']['beverage_supply']
+        update['drinks'] = sensors['beverage_supply']
 
     conn.close()
 
