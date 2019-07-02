8c2c4c97fb9fb461fc3b70c9e2baa67f755e56c6
Felix Möller <git@felix-moeller.net>
2019-07-02 21:55:50 +0200

    Add raw drinks metrics

--- state/graphite/fd-spacestats.py
+++ state/graphite/fd-spacestats.py
@@ -163,6 +163,7 @@ def main():
 
         # drinks storage 0-XX per drink (unit crates)
         update['drinks'] = sensors['beverage_supply']
+        update['drinks_raw'] = sensors['beverage_supply_raw']
 
     conn.close()
 
